a114fd7b79be1ba33656f0c7819647a732961bc4
guonaihong <guonaihong@qq.com>
2014-04-23 23:05:19 +0800

    add the strcmp function of my own

--- call_string.c
+++ call_string.c
@@ -2,11 +2,19 @@
 #include <stdint.h>
 
 int ho_strlen(char *str);
+int ho_strcmp(const char *s1, const char *s2);
 
 int main(int argc, char **argv) {
 
-	while (*++argv) {
-		printf("%d\n", ho_strlen(*argv));
+	int i = 0;
+	/*test ho_strlen*/
+	for (; i < argc; i++) {
+		printf("%d\n", ho_strlen(argv[i]));
 	}
+
+	/*test ho_strcmp*/
+	printf("%d\n", ho_strcmp("12", "123"));
+	printf("%d\n", ho_strcmp("12", "12"));
+	printf("%d\n", ho_strcmp("123", "12"));
 	return 0;
 }
--- ho_string.s
+++ ho_string.s
@@ -1,7 +1,5 @@
 .type ho_strlen, @function
-
 .global ho_strlen
-
 ho_strlen:
 
 	xor   %ecx, %ecx
@@ -18,4 +16,29 @@ ho_string_loop_begin:
 
 ho_string_loop_end:
 	mov   %ecx, %eax
+	ret
+
+.type ho_strcmp, @function
+.global ho_strcmp
+ho_strcmp:
+
+ho_strcmp_loop_begin:
+
+	movb (%rdi), %al
+	movb (%rsi), %cl
+
+	cmpb $0, %al
+	je ho_strcmp_loop_end
+
+	cmpb %al, %cl
+	jne ho_strcmp_loop_end
+
+	inc %rdi
+	inc %rsi
+
+	jmp  ho_strcmp_loop_begin
+
+ho_strcmp_loop_end:
+	subb %cl, %al
+	movsbl %al, %eax
 	ret
